01368c78239f3e7ccd22c82ee69c473124962078
xmic <xmic@fi.muni.cz>
2023-07-06 03:37:18 +0200

    Update CranberryAlgorithm.java

--- src/main/java/vm/search/impl/multiFiltering/CranberryAlgorithm.java
+++ src/main/java/vm/search/impl/multiFiltering/CranberryAlgorithm.java
@@ -37,7 +37,7 @@ import vm.simRel.impl.SimRelEuclideanPCAImplForTesting;
  */
 public class CranberryAlgorithm<T> extends SearchingAlgorithm<T> {
 
-    public static final Integer QUERIES_PARALELISM = vm.javatools.Tools.PARALELISATION;
+    public static final Integer QUERIES_PARALELISM = (int) (Runtime.getRuntime().availableProcessors() / 3f);
     public static final Integer MAX_DIST_COMPS = 1150;
     public static final Boolean STORE_RESULTS = true;
 
