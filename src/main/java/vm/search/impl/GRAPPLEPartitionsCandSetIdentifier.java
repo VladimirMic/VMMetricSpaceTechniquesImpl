e0de6cf4bebd6b76145f605ef76b78fe3570f5c3
VladimirMic <xmic@fi.muni.cz>
2023-08-25 03:04:14 +0200

    Update GRAPPLEPartitionsCandSetIdentifier.java

--- src/main/java/vm/search/impl/GRAPPLEPartitionsCandSetIdentifier.java
+++ src/main/java/vm/search/impl/GRAPPLEPartitionsCandSetIdentifier.java
@@ -5,14 +5,12 @@
 package vm.search.impl;
 
 import java.util.AbstractMap;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import vm.datatools.Tools;
-import vm.datatools.Tools.MapByValueComparatorWithOwnKeyComparator;
 import vm.metricSpace.Dataset;
 import vm.metricSpace.datasetPartitioning.StorageDatasetPartitionsInterface;
 import vm.metricSpace.distance.DistanceFunctionInterface;
@@ -31,48 +29,47 @@ public class GRAPPLEPartitionsCandSetIdentifier<T> extends VoronoiPartitionsCand
     @Override
     public Object[] evaluateKeyOrdering(DistanceFunctionInterface<T> df, Map<Object, T> pivotsMap, T qData, Object... params) {
         Iterator<Map.Entry<Object, T>> it1 = pivotsMap.entrySet().iterator();
-        Iterator<Map.Entry<Object, T>> it2 = pivotsMap.entrySet().iterator();
         SortedSet<AbstractMap.SimpleEntry<Object, Float>> pivotPairs = new TreeSet(new Tools.MapByValueComparator<>());
         Map<String, Float> distsP1P2 = new HashMap<>();
-        while (it1.hasNext()) {
+        for (int idx1 = 0; it1.hasNext(); idx1++) {
             Map.Entry<Object, T> p1 = it1.next();
             Object p1ID = p1.getKey();
             T p1Data = p1.getValue();
-            while (it2.hasNext()) {
-                Map.Entry<Object, T> p2 = it2.next();
-                Object p2ID = p2.getKey();
-                if (p1ID == p2ID) {
+            float distQP1 = df.getDistance(qData, p1Data);
+            Iterator<Map.Entry<Object, T>> it2 = pivotsMap.entrySet().iterator();
+            for (int idx2 = 0; it2.hasNext(); idx2++) {
+                if (idx2 <= idx1) {
+                    it2.next();
                     continue;
                 }
+                Map.Entry<Object, T> p2 = it2.next();
+                Object p2ID = p2.getKey();
                 T p2Data = p2.getValue();
                 String key1 = p1ID + "-" + p2ID;
-                float distQP1 = df.getDistance(qData, p1Data);
                 float distQP2 = df.getDistance(qData, p2Data);
                 Float distP1P2 = distsP1P2.get(key1);
                 if (distP1P2 == null) {
                     distP1P2 = df.getDistance(p1Data, p2Data);
                     distsP1P2.put(key1, distP1P2);
                 }
-                float priority = (distQP2 * distQP2 + distQP1 * distQP1 - distP1P2 * distP1P2) / (2 * distQP1 * distQP2);
+                float priority = 10000 * (distQP2 * distQP2 + distQP1 * distQP1 - distP1P2 * distP1P2) / (2 * distQP1 * distQP2);
                 String key2 = p2ID + "-" + p1ID;
                 if (distQP1 < distQP2) {
-                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key1, priority - Float.MIN_NORMAL));
-                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key2, priority + Float.MIN_NORMAL));
+                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key1, priority - 0.1f));
+                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key2, priority));
                 } else {
-                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key2, priority - Float.MIN_NORMAL));
-                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key1, priority + Float.MIN_NORMAL));
+                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key2, priority - 0.1f));
+                    pivotPairs.add(new AbstractMap.SimpleEntry<>(key1, priority));
                 }
             }
-
         }
-    }
-
-    private class ComparatorForPriorityQueue extends MapByValueComparatorWithOwnKeyComparator<Float> {
-
-        public ComparatorForPriorityQueue(Comparator<Float> comp) {
-            super(comp);
+        Iterator<AbstractMap.SimpleEntry<Object, Float>> it = pivotPairs.iterator();
+        Object[] ret = new Object[pivotPairs.size()];
+        for (int i = 0; it.hasNext(); i++) {
+            AbstractMap.SimpleEntry<Object, Float> entry = it.next();
+            ret[i] = entry.getKey();
         }
-
+        return ret;
     }
 
 }
