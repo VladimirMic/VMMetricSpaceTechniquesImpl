7549c5a290b23451aa5c6e00571dd3018521a790
Vladimir Mic <xmic@fi.muni.cz>
2024-02-14 22:59:28 +0100

    learning pivot pairs ptolemaios - first version

--- src/main/java/vm/metricSpace/distance/bounding/twopivots/learning/LearningPivotPairsForPtolemyInequalityWithLimitedAngles.java
+++ src/main/java/vm/metricSpace/distance/bounding/twopivots/learning/LearningPivotPairsForPtolemyInequalityWithLimitedAngles.java
@@ -5,18 +5,16 @@
 package vm.metricSpace.distance.bounding.twopivots.learning;
 
 import java.util.AbstractMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import vm.datatools.Tools;
 import vm.metricSpace.AbstractMetricSpace;
 import vm.metricSpace.ToolsMetricDomain;
 import vm.metricSpace.distance.DistanceFunctionInterface;
 import vm.metricSpace.distance.bounding.twopivots.impl.DataDependentGeneralisedPtolemaicFiltering;
-import static vm.metricSpace.distance.bounding.twopivots.impl.DataDependentGeneralisedPtolemaicFiltering.CONSTANT_FOR_PRECISION;
-import vm.metricSpace.distance.bounding.twopivots.storeLearned.PtolemyInequalityWithLimitedAnglesCoefsStoreInterface;
 import vm.search.algorithm.SearchingAlgorithm;
 
 /**
@@ -45,28 +43,51 @@ public class LearningPivotPairsForPtolemyInequalityWithLimitedAngles<T> {
         this.filter = filter;
     }
 
-    public Map<Object, float[]> execute() {
-        TreeSet<AbstractMap.Entry<String[], Integer>> filteredCounts = new TreeSet<>(new Tools.MapByValueComparatorWithOwnKeyComparator(new Tools.ObjectArrayIdentityComparator()));
+    public TreeSet<Map.Entry<String, Integer>> execute() {
         Map<Object, Object> oMap = ToolsMetricDomain.getMetricObjectsAsIdObjectMap(metricSpace, sampleObjects, false);
         Object[] sampleQueryArray = ToolsMetricDomain.getData(sampleQueries.toArray(), metricSpace);
-        for (Object qData : sampleQueryArray) {
-            evaluateQuery((T) qData, oMap, filteredCounts);
+        Map<String, Integer> sumsForQueries = new HashMap<>();
+        for (Object sampleQueryArray1 : sampleQueryArray) {
+            Map<String, Integer> evaluatedQuery = evaluateQuery((T) sampleQueryArray1, oMap);
+            for (String key : evaluatedQuery.keySet()) {
+                int value = evaluatedQuery.get(key);
+                if (sumsForQueries.containsKey(key)) {
+                    int value2 = sumsForQueries.get(key);
+                    sumsForQueries.put(key, value + value2);
+                } else {
+                    sumsForQueries.put(key, value);
+                }
+            }
         }
+        TreeSet<Map.Entry<String, Integer>> ret = new TreeSet<>(new Tools.MapByValueComparator());
+        for (Map.Entry<String, Integer> entry : sumsForQueries.entrySet()) {
+            ret.add(entry);
+        }
+        return ret;
     }
-    //       ulozit vysledky - nejlepsi dvojice pivotu
 
-    private void evaluateQuery(T qData, Map<Object, Object> oMap, TreeSet<Map.Entry<String[], Integer>> filteredCounts) {
-        TreeSet<Map.Entry<Object, Float>> answer = new TreeSet<>(new Tools.MapByValueComparator());
+    private Map<String, Integer> evaluateQuery(T qData, Map<Object, Object> oMap) {
+        TreeSet<Map.Entry<Object, Float>> queryAnswer = new TreeSet<>(new Tools.MapByValueComparator());
+        Map<String, Integer> ret = new HashMap<>();
+
+        Object[] pivotsData = ToolsMetricDomain.getData(pivots.toArray(), metricSpace);
+
+        float[] qpDists = new float[pivots.size()];
+        for (int i = 0; i < pivots.size(); i++) {
+            T pData = (T) pivotsData[i];
+            qpDists[i] = df.getDistance(qData, pData);
+        }
+
+        float[][] qp1p2MultipliedByCoef = computeqpDistMultipliedByCoefForPivots(qpDists, pivotsData);
         float range = 0;
         for (Map.Entry<Object, Object> o : oMap.entrySet()) {
             T oData = (T) o.getValue();
-            if (answer.size() < K) {
+            if (queryAnswer.size() < K) {
                 float distance = df.getDistance((T) qData, oData);
-                answer.add(new AbstractMap.SimpleEntry<>(o.getKey(), distance));
-                range = SearchingAlgorithm.adjustAndReturnSearchRadiusAfterAddingOne(answer, K);
+                queryAnswer.add(new AbstractMap.SimpleEntry<>(o.getKey(), distance));
+                range = SearchingAlgorithm.adjustAndReturnSearchRadiusAfterAddingOne(queryAnswer, K);
                 continue;
             }
-
             for (int p1Idx = 0; p1Idx < pivots.size() - 1; p1Idx++) {
                 Object p1 = pivots.get(p1Idx);
                 T p1Data = metricSpace.getDataOfMetricObject(p1);
@@ -76,12 +97,35 @@ public class LearningPivotPairsForPtolemyInequalityWithLimitedAngles<T> {
                     Object p2 = pivots.get(p2Idx);
                     T p2Data = metricSpace.getDataOfMetricObject(p2);
                     Object p2ID = metricSpace.getIDOfMetricObject(p2);
-                    float coef = filter.getCoefPivotPivotForLB(p1Idx, p2Idx);
                     float p2O = df.getDistance(p2Data, oData);
+                    float lb = filter.lowerBound(p2O, qp1p2MultipliedByCoef[p1Idx][p2Idx], p1O, qp1p2MultipliedByCoef[p2Idx][p1Idx]);
+                    if (lb >= range) {
+                        String key = p1ID.toString() + "-" + p2ID.toString();
+                        if (!ret.containsKey(key)) {
+                            ret.put(key, 1);
+                        } else {
+                            Integer value = ret.get(key);
+                            value++;
+                            ret.put(key, value);
+                        }
+                    }
                 }
-
             }
         }
+        return ret;
+    }
+
+    private float[][] computeqpDistMultipliedByCoefForPivots(float[] qpDists, Object[] pivotsData) {
+        float[][] ret = new float[pivotsData.length][pivotsData.length];
+        for (int i = 0; i < pivotsData.length; i++) {
+            float qp1Dist = qpDists[i];
+            for (int j = 0; j < pivotsData.length; j++) {
+                float qp2Dist = qpDists[j];
+                ret[i][j] = qp1Dist * filter.getCoefPivotPivotForLB(i, j);
+                ret[j][i] = qp2Dist * filter.getCoefPivotPivotForLB(j, i);
+            }
+        }
+        return ret;
     }
 
 }
