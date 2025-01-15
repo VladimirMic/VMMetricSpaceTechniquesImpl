d1a52afbce8e592373aa9a3c63d75e28efb9a9a4
Vladimir Mic <xmic@fi.muni.cz>
2025-01-15 18:14:57 +0100

    Update MyBoxAndWhiskerRenderer.java

--- src/main/java/vm/plot/impl/MyBoxAndWhiskerRenderer.java
+++ src/main/java/vm/plot/impl/MyBoxAndWhiskerRenderer.java
@@ -35,9 +35,28 @@ import org.jfree.data.statistics.BoxAndWhiskerCategoryDataset;
  */
 public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
 
+    public static final Boolean DRAW_FAROUTS_AS_OUTLIERS = true;
     public static Integer GAP_TRACES = 28;
     public static Integer GAP_GROUPS = 28;
     public static Integer Y_WIDTH = 160;
+
+//    private Float minDisplayedCategoryValue = null;
+//    private Float maxnDisplayedCategoryValue = null;
+//
+//    public MyBoxAndWhiskerRenderer(Object[] categoryLabels) {
+//        if (Tools.isParseableToFloats(categoryLabels)) {
+//            minDisplayedCategoryValue = Float.valueOf(categoryLabels[0].toString());
+//            minDisplayedCategoryValue = Float.valueOf(categoryLabels[categoryLabels.length - 1].toString());
+//        }
+//    }
+//
+//    public void setMinDisplayedCategoryValue(Float minDisplayedCategoryValue) {
+//        this.minDisplayedCategoryValue = minDisplayedCategoryValue;
+//    }
+//
+//    public void setMaxnDisplayedCategoryValue(Float maxnDisplayedCategoryValue) {
+//        this.maxnDisplayedCategoryValue = maxnDisplayedCategoryValue;
+//    }
 
     private Double outlierRadius;
 
@@ -49,7 +68,6 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
         this.outlierRadius = outlierRadius;
     }
 
-    
     @Override
     public void drawVerticalItem(Graphics2D g2, CategoryItemRendererState state,
             Rectangle2D dataArea, CategoryPlot plot, CategoryAxis domainAxis,
@@ -81,15 +99,12 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
             xx = xx + offset;
         }
 
-        double yyAverage;
-        double yyOutlier;
-
         Paint itemPaint = getItemPaint(row, column);
         g2.setPaint(itemPaint);
         Stroke s = getItemStroke(row, column);
         g2.setStroke(s);
 
-        double aRadius = 0;                 // average radius
+        double aRadius = 0;  // average radius
 
         RectangleEdge location = plot.getRangeAxisEdge();
 
@@ -99,7 +114,6 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
         Number yMin = bawDataset.getMinRegularValue(row, column);
         Shape box = null;
         Paint outlinePaint = getItemOutlinePaint(row, column);
-        Number meanValue = bawDataset.getMeanValue(row, column);
 
         if (yQ1 != null && yQ3 != null && yMax != null && yMin != null) {
 
@@ -139,7 +153,7 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
         if (isMeanVisible()) {
             Number yMean = bawDataset.getMeanValue(row, column);
             if (yMean != null) {
-                yyAverage = rangeAxis.valueToJava2D(yMean.doubleValue(), dataArea, location);
+                double yyAverage = rangeAxis.valueToJava2D(yMean.doubleValue(), dataArea, location);
                 aRadius = state.getBarWidth() / 4;
                 // here we check that the average marker will in fact be
                 // visible before drawing it...
@@ -161,13 +175,10 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
             }
         }
 
-        // draw yOutliers...
+        // draw outliers...
         double maxAxisValue = rangeAxis.valueToJava2D(rangeAxis.getUpperBound(), dataArea, location) + aRadius;
         double minAxisValue = rangeAxis.valueToJava2D(rangeAxis.getLowerBound(), dataArea, location) - aRadius;
-
         g2.setPaint(outlinePaint);
-
-        // draw outliers
         double oRadius = outlierRadius == null ? state.getBarWidth() / 6 : outlierRadius;    // outlier radius
         java.util.List outliers = new ArrayList();
         OutlierListCollection outlierListCollection = new OutlierListCollection();
@@ -177,12 +188,12 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
         // OutlierListCollection
         List yOutliers = bawDataset.getOutliers(row, column);
         if (yOutliers != null) {
+            Number minRegular = bawDataset.getMinRegularValue(row, column);
+            Number maxRegular = bawDataset.getMaxRegularValue(row, column);
             for (int i = 0; i < yOutliers.size(); i++) {
                 double outlier = ((Number) yOutliers.get(i)).doubleValue();
-                Number minRegular = bawDataset.getMinRegularValue(row, column);
-                Number maxRegular = bawDataset.getMaxRegularValue(row, column);
-                if (outlier > maxRegular.doubleValue() || outlier < minRegular.doubleValue()) {
-                    yyOutlier = rangeAxis.valueToJava2D(outlier, dataArea, location);
+                if (DRAW_FAROUTS_AS_OUTLIERS || outlier > maxRegular.doubleValue() || outlier < minRegular.doubleValue()) {
+                    double yyOutlier = rangeAxis.valueToJava2D(outlier, dataArea, location);
                     outliers.add(new Outlier(xx + state.getBarWidth() / 2.0, yyOutlier, oRadius));
                 }
             }
@@ -197,11 +208,11 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
             }
 
             // draw farout indicators
-            if (outlierListCollection.isHighFarOut()) {
+            if (!DRAW_FAROUTS_AS_OUTLIERS && outlierListCollection.isHighFarOut()) {
                 drawHighFarOut(aRadius / 2.0, g2, xx + state.getBarWidth() / 2.0, maxAxisValue);
             }
 
-            if (outlierListCollection.isLowFarOut()) {
+            if (!DRAW_FAROUTS_AS_OUTLIERS && outlierListCollection.isLowFarOut()) {
                 drawLowFarOut(aRadius / 2.0, g2, xx + state.getBarWidth() / 2.0, minAxisValue);
             }
         }
@@ -276,6 +287,161 @@ public class MyBoxAndWhiskerRenderer extends BoxAndWhiskerRenderer {
         g2.draw(new Line2D.Double(xx - side, m - side, xx + side, m - side));
         g2.draw(new Line2D.Double(xx - side, m - side, xx, m));
         g2.draw(new Line2D.Double(xx + side, m - side, xx, m));
+    }
+
+    @Override
+    public void drawHorizontalItem(Graphics2D g2, CategoryItemRendererState state, Rectangle2D dataArea, CategoryPlot plot, CategoryAxis domainAxis, ValueAxis rangeAxis, CategoryDataset dataset, int row, int column) {
+        BoxAndWhiskerCategoryDataset bawDataset
+                = (BoxAndWhiskerCategoryDataset) dataset;
+
+        double categoryEnd = domainAxis.getCategoryEnd(column,
+                getColumnCount(), dataArea, plot.getDomainAxisEdge());
+        double categoryStart = domainAxis.getCategoryStart(column,
+                getColumnCount(), dataArea, plot.getDomainAxisEdge());
+        double categoryWidth = Math.abs(categoryEnd - categoryStart);
+
+        double yy = categoryStart;
+        int seriesCount = getRowCount();
+        int categoryCount = getColumnCount();
+
+        if (seriesCount > 1) {
+            double seriesGap = dataArea.getHeight() * getItemMargin() / (categoryCount * (seriesCount - 1));
+            double usedWidth = (state.getBarWidth() * seriesCount) + (seriesGap * (seriesCount - 1));
+            // offset the start of the boxes if the total width used is smaller
+            // than the category width
+            double offset = (categoryWidth - usedWidth) / 2;
+            yy = yy + offset + (row * (state.getBarWidth() + seriesGap));
+        } else {
+            // offset the start of the box if the box width is smaller than
+            // the category width
+            double offset = (categoryWidth - state.getBarWidth()) / 2;
+            yy = yy + offset;
+        }
+
+        g2.setPaint(getItemPaint(row, column));
+        Stroke s = getItemStroke(row, column);
+        g2.setStroke(s);
+
+        RectangleEdge location = plot.getRangeAxisEdge();
+
+        Number xQ1 = bawDataset.getQ1Value(row, column);
+        Number xQ3 = bawDataset.getQ3Value(row, column);
+        Number xMax = bawDataset.getMaxRegularValue(row, column);
+        Number xMin = bawDataset.getMinRegularValue(row, column);
+
+        Shape box = null;
+        if (xQ1 != null && xQ3 != null && xMax != null && xMin != null) {
+
+            double xxQ1 = rangeAxis.valueToJava2D(xQ1.doubleValue(), dataArea, location);
+            double xxQ3 = rangeAxis.valueToJava2D(xQ3.doubleValue(), dataArea, location);
+            double xxMax = rangeAxis.valueToJava2D(xMax.doubleValue(), dataArea, location);
+            double xxMin = rangeAxis.valueToJava2D(xMin.doubleValue(), dataArea, location);
+            double yymid = yy + state.getBarWidth() / 2.0;
+            double halfW = (state.getBarWidth() / 2.0) * getWhiskerWidth();
+
+            // draw the box...
+            box = new Rectangle2D.Double(Math.min(xxQ1, xxQ3), yy, Math.abs(xxQ1 - xxQ3), state.getBarWidth());
+            if (getFillBox()) {
+                g2.fill(box);
+            }
+
+            Paint outlinePaint = getItemOutlinePaint(row, column);
+            if (getUseOutlinePaintForWhiskers()) {
+                g2.setPaint(outlinePaint);
+            }
+            // draw the upper shadow...
+            g2.draw(new Line2D.Double(xxMax, yymid, xxQ3, yymid));
+            g2.draw(new Line2D.Double(xxMax, yymid - halfW, xxMax, yymid + halfW));
+
+            // draw the lower shadow...
+            g2.draw(new Line2D.Double(xxMin, yymid, xxQ1, yymid));
+            g2.draw(new Line2D.Double(xxMin, yymid - halfW, xxMin, yymid + halfW));
+
+            g2.setStroke(getItemOutlineStroke(row, column));
+            g2.setPaint(outlinePaint);
+            g2.draw(box);
+        }
+
+        // draw mean - SPECIAL AIMS REQUIREMENT...
+        //////////////////////
+        g2.setPaint(getArtifactPaint());
+        double aRadius = 0;  // average radius
+        if (isMeanVisible()) {
+            Number xMean = bawDataset.getMeanValue(row, column);
+            if (xMean != null) {
+                double xxMean = rangeAxis.valueToJava2D(xMean.doubleValue(), dataArea, location);
+                aRadius = state.getBarWidth() / 4;
+                double xxAverage = rangeAxis.valueToJava2D(xMean.doubleValue(), dataArea, location);
+                // here we check that the average marker will in fact be
+                // visible before drawing it...
+                if ((xxMean > (dataArea.getMinX() - aRadius)) && (xxMean < (dataArea.getMaxX() + aRadius))) {
+//                    Ellipse2D.Double avgEllipse = new Ellipse2D.Double(xxMean - aRadius, yy + aRadius, aRadius * 2, aRadius * 2);
+//                    g2.fill(avgEllipse);
+//                    g2.draw(avgEllipse);
+                    g2.draw(new Line2D.Double(yy, xxAverage, yy + state.getBarWidth(), xxAverage));
+                }
+            }
+        }
+
+        // draw median...
+        if (isMedianVisible()) {
+            Number xMedian = bawDataset.getMedianValue(row, column);
+            if (xMedian != null) {
+                double xxMedian = rangeAxis.valueToJava2D(xMedian.doubleValue(), dataArea, location);
+                g2.draw(new Line2D.Double(xxMedian, yy, xxMedian, yy + state.getBarWidth()));
+            }
+        }
+
+        // draw outliers...
+        double maxAxisValue = rangeAxis.valueToJava2D(rangeAxis.getUpperBound(), dataArea, location) + aRadius;
+        double minAxisValue = rangeAxis.valueToJava2D(rangeAxis.getLowerBound(), dataArea, location) - aRadius;
+        Paint outlinePaint = getItemOutlinePaint(row, column);
+        g2.setPaint(outlinePaint);
+        double oRadius = outlierRadius == null ? state.getBarWidth() / 6 : outlierRadius;    // outlier radius
+        java.util.List outliers = new ArrayList();
+        OutlierListCollection outlierListCollection = new OutlierListCollection();
+
+        // From outlier array sort out which are outliers and put these into a
+        // list If there are any farouts, set the flag on the
+        // OutlierListCollection
+        List xOutliers = bawDataset.getOutliers(row, column);
+        if (xOutliers != null) {
+            Number minRegular = bawDataset.getMinRegularValue(row, column);
+            Number maxRegular = bawDataset.getMaxRegularValue(row, column);
+            for (int i = 0; i < xOutliers.size(); i++) {
+                double outlier = ((Number) xOutliers.get(i)).doubleValue();
+                if (DRAW_FAROUTS_AS_OUTLIERS || outlier > maxRegular.doubleValue() || outlier < minRegular.doubleValue()) {
+                    double xxOutlier = rangeAxis.valueToJava2D(outlier, dataArea, location);
+                    outliers.add(new Outlier(yy + state.getBarWidth() / 2.0, xxOutlier, oRadius));
+                }
+            }
+            Collections.sort(outliers);
+
+            // Process outliers. Each outlier is either added to the
+            // appropriate outlier list or a new outlier list is made // clustering
+            for (Iterator iterator = outliers.iterator(); iterator.hasNext();) {
+                Outlier outlier = (Outlier) iterator.next();
+                Point2D point = outlier.getPoint();
+                drawEllipse(point, oRadius, g2);
+            }
+
+            // draw farout indicators
+            if (!DRAW_FAROUTS_AS_OUTLIERS && outlierListCollection.isHighFarOut()) {
+                drawHighFarOut(aRadius / 2.0, g2, yy + state.getBarWidth() / 2.0, maxAxisValue);
+            }
+
+            if (!DRAW_FAROUTS_AS_OUTLIERS && outlierListCollection.isLowFarOut()) {
+                drawLowFarOut(aRadius / 2.0, g2, yy + state.getBarWidth() / 2.0, minAxisValue);
+            }
+        }
+
+        // collect entity and tool tip information...
+        if (state.getInfo() != null && box != null) {
+            EntityCollection entities = state.getEntityCollection();
+            if (entities != null) {
+                addItemEntity(entities, dataset, row, column, box);
+            }
+        }
     }
 
 }
