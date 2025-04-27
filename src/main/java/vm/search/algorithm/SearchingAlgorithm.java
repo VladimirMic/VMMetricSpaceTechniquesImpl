92ba0a759996fb52d37701499cb14aeab3c7259e
VladimirMic <xmic@fi.muni.cz>
2025-04-27 15:12:25 +0200

    Prepared for plots - gradual evaluation of cand set

--- src/main/java/vm/search/algorithm/SearchingAlgorithm.java
+++ src/main/java/vm/search/algorithm/SearchingAlgorithm.java
@@ -246,8 +246,8 @@ public abstract class SearchingAlgorithm<T> {
      */
     public Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> evaluateIteratorsSequentiallyForEachQuery(Dataset dataset, List queryObjects, int k, boolean storePartialCandSetSizes, int candidatesProvided) {
         AbstractMetricSpace metricSpace = dataset.getMetricSpace();
-        Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> ret = initAnswerMapForCandSetSizes(candidatesProvided, queryObjects.size(), storePartialCandSetSizes);
         int batchSize = storePartialCandSetSizes && candidatesProvided > 0 ? candidatesProvided / STEP_COUNTS_FOR_CAND_SE_PROCESSING_FROM_INDEX : candidatesProvided;
+        Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> ret = initAnswerMapForCandSetSizes(candidatesProvided, queryObjects.size(), batchSize);
         for (int i = 0; i < queryObjects.size(); i++) {
             vm.javatools.Tools.sleepDuringTheNight();
             Object q = queryObjects.get(i);
@@ -255,8 +255,9 @@ public abstract class SearchingAlgorithm<T> {
             Iterator candsIt = dataset.getMetricObjectsFromDataset(qID);
             for (int batchCounter = 1; candsIt.hasNext(); batchCounter++) {
                 Iterator<Object> batchIt = Tools.getObjectsFromIterator(candsIt, batchSize).iterator();
+                TreeSet<Map.Entry<Comparable, Float>>[] prev = ret.get((batchCounter - 1) * batchSize);
+                TreeSet completeKnnSearch = completeKnnSearch(metricSpace, q, k, batchIt, prev[i]);
                 TreeSet<Map.Entry<Comparable, Float>>[] retForCandSetSize = ret.get(batchCounter * batchSize);
-                TreeSet completeKnnSearch = completeKnnSearch(metricSpace, q, k, batchIt, retForCandSetSize[i]);
                 retForCandSetSize[i] = completeKnnSearch;
             }
             long timeOfQuery = getTimeOfQuery(qID);
@@ -266,15 +267,10 @@ public abstract class SearchingAlgorithm<T> {
         return ret;
     }
 
-    private Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> initAnswerMapForCandSetSizes(int candidatesProvided, int queriesCount, boolean storePartialCandSetSizes) {
+    private Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> initAnswerMapForCandSetSizes(int candidatesProvided, int queriesCount, int batch) {
         Map<Integer, TreeSet<Map.Entry<Comparable, Float>>[]> ret = new TreeMap<>();
-        if (storePartialCandSetSizes && candidatesProvided > 0) {
-            int batch = candidatesProvided / STEP_COUNTS_FOR_CAND_SE_PROCESSING_FROM_INDEX;
-            for (int i = 1; i <= STEP_COUNTS_FOR_CAND_SE_PROCESSING_FROM_INDEX; i++) {
-                ret.put(i * batch, new TreeSet[queriesCount]);
-            }
-        } else {
-            ret.put(candidatesProvided, new TreeSet[queriesCount]);
+        for (int batchCurr = 0; batchCurr <= candidatesProvided; batchCurr += batch) {
+            ret.put(batchCurr, new TreeSet[queriesCount]);
         }
         return ret;
     }
