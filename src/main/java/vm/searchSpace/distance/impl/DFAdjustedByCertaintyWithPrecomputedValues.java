34a493f92ed17c0458549c0835cf2aeac1dda9b0
VladimirMic <xmic@fi.muni.cz>
2025-11-11 02:05:22 +0100

    storing only second matrix of sth (weights) for modified precomputed dists - dynamic derivation is intended instead of direct storing

--- src/main/java/vm/searchSpace/distance/impl/DFAdjustedByCertaintyWithPrecomputedValues.java
+++ src/main/java/vm/searchSpace/distance/impl/DFAdjustedByCertaintyWithPrecomputedValues.java
@@ -1,5 +1,6 @@
 package vm.searchSpace.distance.impl;
 
+import vm.datatools.Tools;
 import vm.searchSpace.Dataset;
 import vm.searchSpace.distance.storedPrecomputedDistances.MainMemoryStoredPrecomputedDistances;
 
@@ -10,8 +11,13 @@ import vm.searchSpace.distance.storedPrecomputedDistances.MainMemoryStoredPrecom
  */
 public class DFAdjustedByCertaintyWithPrecomputedValues<T> extends DFWithPrecomputedValues<T> {
 
+    public static final String NAME_PREFFIX = "AdjustedByCertaintySym";
     private double power = 2;
-    public static final String NAME_PREFFIX = "AdjustedByCertaintySym_pow";
+    public final float[][] origDists;
+
+    public DFAdjustedByCertaintyWithPrecomputedValues(Dataset dataset, MainMemoryStoredPrecomputedDistances distsHolder, float[][] weights) {
+        this(dataset, distsHolder, weights, 2);
+    }
 
     /**
      *
@@ -23,31 +29,49 @@ public class DFAdjustedByCertaintyWithPrecomputedValues<T> extends DFWithPrecomp
     public DFAdjustedByCertaintyWithPrecomputedValues(Dataset dataset, MainMemoryStoredPrecomputedDistances distsHolder, float[][] weights, double power) {
         super(dataset, distsHolder, NAME_PREFFIX);
         this.power = power;
-        float max = -1;
-        float[][] dists = distsHolder.getDists();
-        for (int i = 0; i < dists.length; i++) {
-            for (int j = 0; j < dists[i].length; j++) {
-                float newDist = (float) (dists[i][j] * (1 + Math.pow(1 - weights[i][j], power)));
-                max = Math.max(max, newDist);
-                distsHolder.modify(i, j, newDist);
-            }
-        }
-        for (int i = 0; i < dists.length; i++) {
-            for (int j = 0; j < dists[i].length; j++) {
-                float newDist = dists[i][j] / max;
+        this.origDists = Tools.copyArray(distsHolder.getDists());
+//        float max = -1;
+//        float[][] dists = distsHolder.getDists();
+//        for (int i = 0; i < dists.length; i++) {
+//            for (int j = 0; j < dists[i].length; j++) {
+//                float newDist = (float) (dists[i][j] * (1 + Math.pow(1 - origDists[i][j], power)));
+//                max = Math.max(max, newDist);
+//                distsHolder.modify(i, j, newDist);
+//            }
+//        }
+        for (int i = 0; i < weights.length; i++) {
+            for (int j = 0; j < weights[i].length; j++) {
+                float newDist = 1 - weights[i][j];
                 distsHolder.modify(i, j, newDist);
             }
         }
     }
 
-    public DFAdjustedByCertaintyWithPrecomputedValues(Dataset dataset, MainMemoryStoredPrecomputedDistances distsHolder, double power) {
-        super(dataset, distsHolder, NAME_PREFFIX);
-        this.power = power;
+//    public DFAdjustedByCertaintyWithPrecomputedValues(Dataset dataset, MainMemoryStoredPrecomputedDistances origDists, float[][] origDists, double power) {
+//        super(dataset, origDists, NAME_PREFFIX);
+//        this.origDists = origDists;
+//        this.power = power;
+//    }
+    @Override
+    public String getSuffix() {
+        return "";
     }
 
     @Override
-    public String getSuffix() {
-        return Double.toString(power);
+    public float getDistance(T obj1, T obj2) {
+        Comparable o1ID = Tools.hashArray(obj1);
+        Comparable o2ID = Tools.hashArray(obj2);
+        String o1IDString = o1ID.toString();
+        String o2IDString = o2ID.toString();
+        if (newColumnHeaders.containsKey(o1IDString) && newRowHeaders.containsKey(o2IDString)) {
+            int o1idx = newColumnHeaders.get(o1IDString);
+            int o2idx = newRowHeaders.get(o2IDString);
+            float weight = distsHolder.getDists()[o1idx][o2idx];
+            float orig = origDists[o1idx][o2idx];
+            float newDist = (float) (orig * (1 + Math.pow(weight, power)));
+            return newDist;
+        }
+        return df.getDistance(obj1, obj2);
     }
 
 }
